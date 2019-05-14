b7cb69e7fb0cdd65a6f84c85475e5085b4eecedf
@gnatyk.anastasia <gnatyk.anastasia@codex-soft.com>
2019-05-14 21:46:52 +0300

    implement edit skill

--- frontend/src/components/SkillsPage.vue
+++ frontend/src/components/SkillsPage.vue
@@ -31,7 +31,7 @@
         <div class="text-xs-center">
           <v-chip outline color="primary" close @input="deleteSkill(skill.name)">
             {{ skill.name }}
-            <v-icon class="edit-icon" size="20" right>edit</v-icon>
+            <v-icon @click="edit(skill.name)" class="edit-icon" size="20" right>edit</v-icon>
           </v-chip>
         </div>
       </div>
@@ -45,7 +45,8 @@ import * as authService from "../services/auth.service";
 export default {
   data: () => ({
     newSkill: "",
-    dialog: false,
+    editSkill: "",
+    dialog: false
   }),
   computed: {
     ...mapGetters("user", { userProfile: "getUserProfile" })
@@ -55,13 +56,20 @@ export default {
       console.log("delete");
     },
 
+    edit(name) {
+      this.newSkill = name;
+      this.editSkill = name;
+      this.dialog = true;
+    },
+
     close() {
       this.dialog = false;
       setTimeout(() => {
         this.newSkill = "";
       }, 300);
     },
-      getUserProfile() {
+
+    getUserProfile() {
       authService.getUserById(this.$store.state.user.logedUserId).then(
         data => {
           this.$store.dispatch("user/setUser", data);
@@ -73,14 +81,26 @@ export default {
     },
 
     save() {
-      const userProfile = JSON.parse(JSON.stringify(this.$store.state.user.userProfile));
+      const userProfile = JSON.parse(
+        JSON.stringify(this.$store.state.user.userProfile)
+      );
+      const currentSkill = userProfile.skills.find(
+        skill => skill.name === this.editSkill
+      );
+      const index = userProfile.skills.indexOf(currentSkill);
+      if (index >= 0) {
+        userProfile.skills.splice(index, 1, { name: this.newSkill });
+      } else {
+        userProfile.skills.push({ name: this.newSkill });
+      }
       const userId = this.$store.state.user.logedUserId;
-      const newProfile = { ...userProfile, skills: [ ...userProfile.skills, {name: this.newSkill} ]}
-      if(this.newSkill) {
-         authService.createNewSkill(userId, newProfile).then(
+      const newProfile = { skills: [...userProfile.skills] };
+      if (this.newSkill) {
+        authService.createNewSkill(userId, newProfile).then(
           () => {
             this.getUserProfile();
             this.close();
+            this.editSkill = "";
           },
           error => {
             console.log(error, "error");
