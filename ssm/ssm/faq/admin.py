a15c546d37c07fd7b871507df3bc1ed0eda78007
twistedFantasy <twisteeed.fantasy@gmail.com>
2019-06-04 14:51:15 +0300

    Rename field fix

--- ssm/ssm/faq/admin.py
+++ ssm/ssm/faq/admin.py
@@ -5,9 +5,9 @@ from ssm.faq.models import FAQ
 
 class FAQAdmin(admin.ModelAdmin):
     list_display = ['question', 'active']
-    search_fields = ['question', 'description']
+    search_fields = ['question', 'answer']
     fieldsets = [
-        (None, {'fields': ['question', 'description', 'order', 'active']})
+        (None, {'fields': ['question', 'answer', 'order', 'active']})
     ]
 
 
