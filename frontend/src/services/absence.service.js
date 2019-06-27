855333eb3fcbe689b5f218dba83381be70b2e4d7
@gnatyk.anastasia <gnatyk.anastasia@codex-soft.com>
2019-06-27 19:59:09 +0300

    Fix absences

--- frontend/src/services/absence.service.js
+++ frontend/src/services/absence.service.js
@@ -4,13 +4,12 @@ const http = require('./http.init');
 
 export const getAllAbsencesByUserId = (dataFilter) => {
   const filter = dataFilter || '';
-  return http.get(`${API_URL}/api/v1/absences/`, filter);
+  return http.get(`${API_URL}/api/v1/absences/${filter}`);
 }
 
 export const createNewAbsence = (data) => {
   return http.post(`${API_URL}/api/v1/absences/`, data);
 }
-
 
 export const editAbsence = (data, id) => {
   return http.patch(`${API_URL}/api/v1/absences/${id}/`, data);
