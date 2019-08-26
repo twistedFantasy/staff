d38467a6df3a010fdb4fb439d1d45a33c2e54abf
gnatyk.anastasia <gnatyk.anastasia@codex-soft.com>
2019-08-26 18:17:53 +0300

    added data pikers

--- frontend/src/components/FiltersBar.vue
+++ frontend/src/components/FiltersBar.vue
@@ -19,22 +19,52 @@
         @change="setFilter({status: $event})"
         single-line
       ></v-select>
-      <v-text-field
-        v-model="filter.start_date"
-        append-icon="search"
-        label="yyyy-mm-dd"
-        @change="setFilter({start_date: $event})"
-        single-line
-        hide-details
-      ></v-text-field>
-      <v-text-field
-        v-model="filter.end_date"
-        append-icon="search"
-        label="yyyy-mm-dd"
-        @change="setFilter({end_date: $event})"
-        single-line
-        hide-details
-      ></v-text-field>
+      <v-flex>
+        <v-menu
+          ref="menu1"
+          :close-on-content-click="false"
+          v-model="menu1"
+          :nudge-right="40"
+          :return-value.sync="filter.start_date"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          min-width="390px"
+        >
+          <v-text-field
+            slot="activator"
+            v-model="filter.start_date"
+            label="Start date"
+            prepend-icon="event"
+            readonly
+          ></v-text-field>
+          <v-date-picker v-model="filter.start_date" @input="$refs.menu1.save(filter.start_date);setFilter({start_date: filter.start_date}) "></v-date-picker>
+        </v-menu>
+      </v-flex>
+         <v-flex>
+        <v-menu
+          ref="menu2"
+          :close-on-content-click="false"
+          v-model="menu2"
+          :nudge-right="40"
+          :return-value.sync="filter.end_date"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          min-width="390px"
+        >
+          <v-text-field
+            slot="activator"
+            v-model="filter.end_date"
+            label="End date"
+            prepend-icon="event"
+            readonly
+          ></v-text-field>
+          <v-date-picker v-model="filter.end_date" @input="$refs.menu2.save(filter.end_date);setFilter({end_date: filter.end_date}) "></v-date-picker>
+        </v-menu>
+      </v-flex>
     </div>
     <v-icon @click="clearFilter()">clear</v-icon>
   </div>
@@ -42,36 +72,28 @@
 
 <script>
 import * as config from "@/config.js";
-import { mapState } from 'vuex';
+import { mapState } from "vuex";
 
 export default {
   props: {
-    setFilter: { type: Function },
+    setFilter: { type: Function }
   },
   data: () => ({
+    menu1: false,
+    menu2: false,
     absenceReasonOptions: config.absenceReasonOptions,
     absenceStatusOptions: config.absenceStatusOptions
   }),
-  
+
   computed: mapState({
     filter: state => state.absence.filter,
-    paginationInfo: state => state.absence.paginationInfo,
+    paginationInfo: state => state.absence.paginationInfo
   }),
 
   methods: {
-    /*selectedDateFilterValue(val) {
-      if (val) {
-        const re = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/g;
-        const isValid = re.test(val);
-        if (isValid) {
-          this.createFilter();
-        }
-      }
-    },*/
-
     clearFilter() {
       this.setFilter({});
-    },
+    }
   }
 };
 </script>
@@ -86,5 +108,14 @@ export default {
 .right-block {
   display: grid;
   grid-template-columns: 9fr 1fr;
+}
+.theme--light.v-input:not(.v-input--is-disabled) input {
+  color: #66A4D4;
+}
+.v-menu__content {
+  box-shadow: none!important;
+}
+ .v-picker__title.primary {
+   background-color: #003851 !important;
 }
 </style>
